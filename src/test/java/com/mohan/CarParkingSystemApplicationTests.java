d6496b2ace5939fdf6f0af61790dea5f9f8ce31e
Pravin Patil <pravinspatil2045@gmail.com>
2021-09-27 09:05:45 +0530

    Initial commit

---
+++ src/test/java/com/mohan/CarParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class CarParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
